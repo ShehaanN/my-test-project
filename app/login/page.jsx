df20c9faa527cf435fac93eca79fa1d80eea90a7
Shehan5498 <shehannadeesha2580@gmail.com>
2024-12-30 22:37:17 +0530

    Add login page

---
+++ app/login/page.jsx
@@ -0,0 +1,7 @@
+export default function Loginpage() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full min-h-screen bg-green-600 text-center">
+      <div className="bg-yellow-500 px-5 py-3 rounded-lg">Login</div>
+    </div>
+  );
+}
